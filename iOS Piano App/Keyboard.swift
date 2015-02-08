851ba6daa7bae9f6d8cd1f078b765e13ae703a50
Brian To <bxt5647@rit.edu>
2015-02-08 12:57:47 -0500

    Add keyboard stuff to scene

--- iOS Piano App/Keyboard.swift
+++ iOS Piano App/Keyboard.swift
@@ -11,12 +11,12 @@ import SpriteKit
 class Keyboard {
     let keyCount = 25 // 2 octaves + 1 note
     let startNote = 60 // Middle C
-    let halfTones = [1, 3, 6, 8, 10]
+    
+    let strokeColor = UIColor.grayColor()
     
     var screenWidth: CGFloat = 0
     var keyWidth: CGFloat = 0
     var keyHeight: CGFloat = 0
-    var keyYPos: CGFloat = 0
     
     let keys: [Key] = []
     
@@ -29,11 +29,14 @@ class Keyboard {
     }
     
     func makeShape(key: Key, scene: SKScene) -> SKShapeNode {
-        let xpos = CGFloat(keyIndex(key)) * CGFloat(keyWidth) + (CGFloat(keyWidth) / 2)
-        let rect = CGRect(x: xpos, y: keyYPos, width: keyWidth, height: keyHeight)
+        let xpos = CGFloat(keyIndex(key)) * CGFloat(keyWidth)
+        let rect = CGRect(x: xpos, y: 0, width: keyWidth, height: keyHeight)
         let shape = SKShapeNode(rect: rect)
         
-        shape.fillColor = keyColor(key)
+        shape.fillColor = key.color()
+        shape.strokeColor = strokeColor
+        
+        key.shape = shape
         
         return shape
     }
@@ -42,28 +45,39 @@ class Keyboard {
         return key.value - startNote
     }
     
-    func keyColor(key: Key) -> UIColor {
-        if contains(halfTones, key.value % 12) {
-            return UIColor.blackColor()
-        }
-        
-        return UIColor.whiteColor()
-    }
-    
     func attach(scene: SKScene) {
         self.scene = scene
         
         self.screenWidth = scene.size.width
         self.keyWidth = screenWidth / CGFloat(keyCount)
         self.keyHeight = CGFloat(0.15) * scene.size.height
-        self.keyYPos = keyHeight / CGFloat(2)
+        
+        for key in keys {
+            var shape = makeShape(key, scene: scene)
+            self.scene?.addChild(shape)
+        }
     }
 }
 
 class Key {
+    let halfTones = [1, 3, 6, 8, 10]
+
     let value: Int
+    var shape: SKShapeNode?
     
     init(value: Int) {
         self.value = value
+    }
+    
+    func isHalfTone() -> Bool {
+        return contains(halfTones, self.value % 12)
+    }
+    
+    func color() -> UIColor {
+        if isHalfTone() {
+            return UIColor.blackColor()
+        }
+        
+        return UIColor.whiteColor()
     }
 }
